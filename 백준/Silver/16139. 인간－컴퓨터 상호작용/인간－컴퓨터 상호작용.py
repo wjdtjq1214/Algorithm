3a0379355e917b46bf577a25a3de17e0bddab876
IMZZ <44230078+wjdtjq1214@users.noreply.github.com>
2025-04-19 01:16:08 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 836 ms, Memory: 205688 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,8 +1,36 @@
-word = input()
+import sys
 
-count = int(input())
+input = sys.stdin.readline
+objList = []
+
+word = input().rstrip()
+
+for i in range(len(word)):
+  char = word[i]
+  newObj = {} if i == 0 else objList[i - 1].copy()
+
+  if char in newObj:
+    newObj[char] += 1
+  else:
+    newObj[char] = 1
+
+  objList.append(newObj)
+
+count = int(input().rstrip())
 
 for _ in range(count):
-  char, start, end = input().split()
+  char, start, end = input().strip().split()
 
-  print(word[int(start): int(end) + 1].count(char))
+  if start == '0':
+    if char in objList[int(end)]:
+      print(objList[int(end)][char])
+    else:
+      print(0)
+  else:
+    if char not in objList[int(end)]:
+      print(0)
+    else:
+      if char not in objList[int(start) - 1]:
+        print(objList[int(end)][char])
+      else:
+        print(objList[int(end)][char] - objList[int(start) - 1][char])
