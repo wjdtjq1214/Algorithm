b9ed342938097ea868f6b9f270e49c2e444946fc
IMZZ <44230078+wjdtjq1214@users.noreply.github.com>
2025-04-23 21:23:54 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.61 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,14 +1,8 @@
 function solution(n) {
-    const countArr = []
-    
-    for (let i = 0; i <= n; i++)
-        countArr.push(0)
-    
-    countArr[0] = BigInt(1)
-    countArr[1] = BigInt(1)
+    const countArr = [BigInt(1), BigInt(1)]
     
     for (let i = 2; i <= n; i++)
-        countArr[i] = countArr[i - 2] + countArr[i - 1]
+        countArr.push(countArr[i - 2] + countArr[i - 1])
     
     return countArr[n] % BigInt(1234567)
 }
