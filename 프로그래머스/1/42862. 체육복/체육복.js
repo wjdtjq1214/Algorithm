222428462cc03767750ac7bca1a9226669bec8a4
IMZZ <44230078+wjdtjq1214@users.noreply.github.com>
2025-05-06 23:35:35 +0900

    [level 1] Title: 체육복, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -0,0 +1,26 @@
+function solution(n, lost, reserve) {
+    const lostArr = []
+    const reserveObj = {}
+    let finalLostCount = 0
+    
+    for (const reserveNum of reserve) reserveObj[reserveNum] = true
+    
+    for (const lostNum of lost) {
+        if (reserveObj[lostNum])
+            reserveObj[lostNum] = false
+        else
+            lostArr.push(lostNum)
+    }
+    
+    
+    
+    lostArr.sort((a, b) => a - b)
+    
+    for (const lostNum of lostArr) {
+        if (reserveObj[lostNum - 1]?? false == true) reserveObj[lostNum - 1] = false
+        else if (reserveObj[lostNum + 1]?? false == true) reserveObj[lostNum + 1] = false
+        else finalLostCount++
+    }
+    
+    return n - finalLostCount
+}
