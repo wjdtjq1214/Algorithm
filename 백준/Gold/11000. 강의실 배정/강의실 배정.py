150643b5e11a03542562eb7675090d141630714a
IMZZ <44230078+wjdtjq1214@users.noreply.github.com>
2025-06-14 12:50:36 +0900

    [Gold V] Title: 강의실 배정, Time: 400 ms, Memory: 88836 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -0,0 +1,23 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+count = int(input())
+
+startEndTimeList = []
+roomHeap = []
+
+for _ in range(count):
+  startEndTimeList.append(list(map(int, input().split())))
+
+startEndTimeList.sort(key=lambda x: (x[0], x[1]))
+
+for [start, end] in startEndTimeList:
+  if len(roomHeap) == 0 or roomHeap[0] > start:
+    heapq.heappush(roomHeap, end)
+  else:
+    heapq.heappop(roomHeap)
+    heapq.heappush(roomHeap, end)
+
+print(len(roomHeap))
