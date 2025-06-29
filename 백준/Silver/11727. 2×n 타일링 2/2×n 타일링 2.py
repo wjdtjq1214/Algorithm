a9798838d12fceccca1c5e8a321619a603e4ed1f
IMZZ <44230078+wjdtjq1214@users.noreply.github.com>
2025-06-29 19:30:30 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 44 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -0,0 +1,15 @@
+n = int(input().rstrip())
+
+countList = [0 for _ in range(n + 1)]
+
+countList[0] = 1
+
+for i in range(1, n + 1):
+  if i == 1:
+    countList[i] = countList[i - 1] % 10007
+
+    continue
+
+  countList[i] = (countList[i - 1] + (countList[i - 2]) * 2) % 10007
+
+print(countList[n])
